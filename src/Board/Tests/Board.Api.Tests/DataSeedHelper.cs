ced88a89c8e482f7ca87238bc7131d698d94f7cc
MeriS13 <mika.milkiway@gmail.com>
2023-04-22 05:33:03 +0300

    написаны 2 интеграционных теста на сервис постов для метода GetById

---
+++ src/Board/Tests/Board.Api.Tests/DataSeedHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Board.Domain.Posts;
+using Board.Domain.Categories;
+using Board.Infrastructure.DataAccess;
+using Board.Infrastructure.DataAccess;
+using Xunit;
+using Board.Domain.ParentCategories;
+using Board.Domain.Accounts;
+
+namespace Board.Api.Tests
+{
+    public static class DataSeedHelper
+    {
+        public static Guid TestParentCategoryId { get; set; }
+        public static Guid TestPostId { get; set; }
+        public static Guid TestCategoryId { get; set; }
+        public static Guid TestAccountUserId { get; set; }
+
+        public static void InitializeDbForTests(BoardDbContext db)
+        {
+            var testAccountUser = new Account
+            {
+                Email = "user@test.com",
+                Name = "User",
+                Login = "User",
+                Password = "useruser",
+                Created = DateTime.UtcNow,
+            };
+            db.Add(testAccountUser);
+
+
+            var testParentCategory = new ParentCategory
+            {
+                Name = "test_ParCat_1",
+            };
+            db.Add(testParentCategory);
+
+
+            var testCategory = new Category
+            {
+                ParentId = testParentCategory.Id,
+                Name = "test_cat_1",
+            };
+            db.Add(testCategory);
+
+
+            var Post = new Post
+            {
+                Name = "test_post_name1",
+                Description = "test_desc1",
+                CreationDate = DateTime.UtcNow,
+                CategoryId = testCategory.Id,
+                AccountId = testAccountUser.Id,
+                IsFavorite = true,
+                
+            };
+            db.Add(Post);
+
+
+            db.SaveChanges();
+
+            TestCategoryId = testCategory.Id;
+            TestPostId = Post.Id;
+            TestParentCategoryId = testParentCategory.Id;
+            TestAccountUserId = testAccountUser.Id;
+        }
+    }
+}
--- src/Board/Tests/Board.Api.Tests/PostTests.cs
+++ src/Board/Tests/Board.Api.Tests/PostTests.cs
@@ -3,14 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Board.Api.Tests
 {
-    /*
-    public class PostTests
+    
+    public class PostTests : IClassFixture<BoardWebApplicationFactory>
     {
         public PostTests(BoardWebApplicationFactory webApplicationFactory)
         {
@@ -19,29 +20,55 @@ namespace Board.Api.Tests
 
         private readonly BoardWebApplicationFactory _webApplicationFactory;
 
+
+        /// <summary>
+        /// Проверка что при введенном идентификаторе все ок
+        /// </summary>
+        /// <returns></returns>
         [Fact]
-        public Task Post_GetById_Success()
+        public async Task Post_GetById_Success()
         {
             // Arrange
             var httpClient = _webApplicationFactory.CreateClient();
             var id = DataSeedHelper.TestPostId;
 
             // Act
-            var response = await httpClient.GetAsync($"Post/{id}");
+            var response = await httpClient.GetAsync($"posts-controller/{id}");
+
+            var result = await response.Content.ReadFromJsonAsync<PostDto>();
 
             // Assert
 
             Assert.NotNull(response);
 
-            var result = await response.Content.ReadFromJsonAsync<PostDto>();
+            
 
             Assert.NotNull(result);
-
-            Assert.Equal("test_advert_name", result!.Name);
-            Assert.Equal("test_desc", result.Description);
-            Assert.True(result.IsActive);
+            Assert.Equal("test_post_name1", result!.Name);
+            Assert.Equal("test_desc1", result.Description);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
+
+        /// <summary>
+        /// Проверка что при неверном идентификаторе генерируется исключение
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task Post_GetById_Fail()
+        {
+            // Arrange
+            var httpClient = _webApplicationFactory.CreateClient();
+            var id = Guid.Parse("d5d57071-5a64-4461-af51-de55676ea625");
+
+            // Act
+            var response = await httpClient.GetAsync($"posts-controller/{id}");
+
+            // Assert
+
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+        }
+
+
     }
-    */
+    
 }
